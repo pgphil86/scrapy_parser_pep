de322c79f91bbbced6249aece095f24e6251be71
Павел Филиппов <pgphil@yandex.ru>
2023-12-16 00:47:27 +0300

    Correct pipelines.py and pep.py.

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -7,39 +7,34 @@ BASE_DIR = Path(__file__).parent.parent
 
 
 class PepParsePipeline:
-    '''
+    """
     PEP documents with different statuses are summarized. At the end, a csv file is created.
-    '''
-
+    """
     def open_spider(self, spider):
-        '''
+        """
         Creating a dictionary.
-        '''
-
+        """
         self.status_count = defaultdict(int)
 
     def process_item(self, item, spider):
-        '''
+        """
         PEP account in different statuses.
-        '''
-
-        status = item['status']
-        self.status_count[status] += 1
+        """
+        self.status_count[item['status']] += 1
         return item
 
     def close_spider(self, spider):
-        '''
+        """
         Creating a csv file.
-        '''
-
+        """
         time_now = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-        filename = BASE_DIR / 'results' / f'status_summary_{time_now}.csv'
+        filename = BASE_DIR/'results'/f'status_summary_{time_now}.csv'
         heading = ('Статус', 'Количество')
         total = 'Total'
-        with open(filename, mode='w', encoding='utf-8') as f:
+        with open(filename, mode='w', encoding='utf-8', newline='') as f:
             writer = csv.writer(f)
             writer.writerows([
                 heading,
-                *(self.status_count.items()),
+                *self.status_count.items(),
                 [total, sum(self.status_count.values())]
             ])
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -4,33 +4,31 @@ from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
-    '''
+    """
     Spider for working with PEP documents.
-    '''
-
+    """
     name = 'pep'
     allowed_domains = ['peps.python.org']
     start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        '''
+        """
         Working with links from PEP documents.
-        '''
-
-        pages = response.css('section[id=numerical-index]').css('a[href^="pep-"]')
-        for link in pages:
-            yield response.follow(link, callback=self.parse_pep)
+        """
+        yield from response.follow_all(
+            css='section[id=numerical-index] a[href^="pep-"]',
+            callback=self.parse_pep
+            )
 
     def parse_pep(self, response):
-        '''
+        """
         Working with a separate PEP page.
-        '''
-
-        page = response.css('section[id=pep-page-section]')
-        title = response.css('h1.page-title::text').get()
+        """
         data = {
-            'number': int(page.css('li::text')[2].get().replace('PEP', '')),
-            'name': title.partition('- ')[2],
+            'number': int(response.css
+                          ('section[id=pep-page-section] li::text')
+                          [2].get().replace('PEP', '').strip()),
+            'name': response.css('h1.page-title::text').get().partition('- ')[2],
             'status': response.css('dt:contains("Status") + dd ::text').get(),
         }
         yield PepParseItem(data)
