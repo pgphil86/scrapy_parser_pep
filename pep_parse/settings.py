acd2e4d5900093249cf12e7094f4779618b15492
Павел Филиппов <pgphil@yandex.ru>
2023-12-16 01:12:08 +0300

    Correct pipelines.py and pep.py.

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -9,11 +9,13 @@ REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
-FEEDS = {f'results/pep_%(time)s.csv': {'format': 'csv', 'fields':
-                                       ['number', 'name', 'status'],
-                                       'overwrite': True},
+FEEDS = {
+    'results/pep_%(time)s.csv': {
+        'format': 'csv',
+        'fields': ['number', 'name', 'status'],
+        'overwrite': True
+    }
 }
-
 ITEM_PIPELINES = {
     'pep_parse.pipelines.PepParsePipeline': 300,
 }
